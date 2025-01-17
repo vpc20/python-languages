9d63161910d6b26eb00fda6a95f09a79ef4dc508
vpc20 <cuevasvp@yahoo.com>
2025-01-17 22:10:12 +0800

    revision

--- random_data.py
+++ random_data.py
@@ -1,9 +1,15 @@
-import random
+from random import choice
+from random import randrange
 
+
+# def random_binary_string(strlen):
+#     length = random.randint(0, strlen)
+#     return ''.join(random.choice('01') for _ in range(length))
 
 def random_binary_string(strlen):
-    length = random.randint(0, strlen)
-    return ''.join(random.choice('01') for _ in range(length))
+    length = randrange(strlen)
+    return ''.join(choice('01') for _ in range(length))
 
 
-print(random_binary_string(1))
+if __name__ == '__main__':
+    print(random_binary_string(2))
